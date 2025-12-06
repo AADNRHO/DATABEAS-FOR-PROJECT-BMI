cadbb71cb9aded0052a3ad779b3378f11f7f9d8b
DOAA ALHELAIY <alkhansaalhelaly@gmail.com>
2025-12-05 15:40:08 -0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Form3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        
+
+        #endregion
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridRecords = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridRecords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Controls.Add(this.dataGridRecords);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(50, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 352);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridRecords
+            // 
+            this.dataGridRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridRecords.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridRecords.Location = new System.Drawing.Point(0, 0);
+            this.dataGridRecords.Name = "dataGridRecords";
+            this.dataGridRecords.ReadOnly = true;
+            this.dataGridRecords.RowHeadersWidth = 51;
+            this.dataGridRecords.RowTemplate.Height = 24;
+            this.dataGridRecords.Size = new System.Drawing.Size(600, 352);
+            this.dataGridRecords.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Tajawal", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(260, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(207, 80);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "السجل";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(700, 506);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tajawal", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form3";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridRecords)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridRecords; // <--- تعريف نهائي وحيد
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadRecords();
+        }
+        
+        private void LoadRecords()
+        {
+            try
+            {
+                // تأكد أن الجدول موجود
+                if (dataGridRecords == null)
+                {
+                    MessageBox.Show("DataGridView غير موجود في الفورم", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+             //   using (var db = new AppDbContext())
+                {
+                 //   var records = db.Measurements
+                                 //   .OrderByDescending(r => r.Date)
+                                 //   .ToList();
+
+                    dataGridRecords.AutoGenerateColumns = true;
+                    dataGridRecords.DataSource = null;
+                //    dataGridRecords.DataSource = records;
+
+                    // حماية من الأخطاء
+                    if (dataGridRecords.Columns.Count > 0)
+                    {
+                        HideColumnIfExists("Id");
+                        RenameColumnIfExists("UserName", "الاسم");
+                        RenameColumnIfExists("Date", "التاريخ");
+                        RenameColumnIfExists("Height", "الطول (سم)");
+                        RenameColumnIfExists("Weight", "الوزن (كجم)");
+                        RenameColumnIfExists("BMIValue", "BMI");
+                        RenameColumnIfExists("BMICategory", "الحالة الصحية");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("تفاصيل الخطأ:\n" + ex.Message, "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // ----------------------------------------
+        // دوال أمان للأعمدة
+        // ----------------------------------------
+        private void HideColumnIfExists(string columnName)
+        {
+            if (dataGridRecords.Columns.Contains(columnName))
+                dataGridRecords.Columns[columnName].Visible = false;
+        }
+
+        private void RenameColumnIfExists(string columnName, string newName)
+        {
+            if (dataGridRecords.Columns.Contains(columnName))
+                dataGridRecords.Columns[columnName].HeaderText = newName;
+        }
+    }
+}
+
+
+   
